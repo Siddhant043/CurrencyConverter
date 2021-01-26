385bef7710b0f4c6b52effdfdbf25616e816db5d
Siddhant Mishra <siddhant48311@gmail.com>
2021-01-26 18:09:36 +0530

    Error handling feature added

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -15,13 +15,26 @@ public class MainActivity extends AppCompatActivity {
     public void convertFunction(View view){
         Log.i("Info", "It works!");
         EditText dollarAmt = (EditText) findViewById(R.id.dollarValue);
-        double dollarAmtInDouble = Double.parseDouble(dollarAmt.getText().toString());
-        double tempValue = dollarAmtInDouble * 72.96;
+        Boolean numeric = true;
+        try{
+            double dollarAmtInDouble = Double.parseDouble(dollarAmt.getText().toString());
+        }catch(NumberFormatException e){
+            numeric = false;
+        }
+
+        if(numeric){
+            double dollarAmtInDouble = Double.parseDouble(dollarAmt.getText().toString());
+            double tempValue = dollarAmtInDouble * 72.96;
 //        DecimalFormat df = new DecimalFormat("#.##");
 //        double rupeesAmtInDouble = Double.valueOf(df.format(tempValue));
-        String rupeesAmtInDouble = String.format("%.2f", tempValue);
-        Log.i("Values", rupeesAmtInDouble);
-        Toast.makeText(this, "Rs." + rupeesAmtInDouble, Toast.LENGTH_SHORT).show();
+            String rupeesAmtInDouble = String.format("%.2f", tempValue);
+            Log.i("Values", "Rs. " + rupeesAmtInDouble);
+            Toast.makeText(this, "Rs. " + rupeesAmtInDouble, Toast.LENGTH_LONG).show();
+        }else {
+            Log.i("text", "Wrong Value entered.");
+            Toast.makeText(this, "Wrong Value Entered", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
